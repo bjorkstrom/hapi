bf915177b21c81bd3754103cc3a89f9b124245a4
elmir <elmir@brab>
2017-11-01 08:08:48 +0100

    ads

--- host/models.py
+++ host/models.py
@@ -4,16 +4,34 @@ from dbmodels import Model
 import database
 
 
-def _parse_model_bson(bson_bytes):
-    return bson.loads(bson_bytes)
+class ParseException(Exception):
+    pass
+
+
+def _invalid_model_resp(ex):
+    return dict(message=str(ex)), 400
 
 
 def _model_not_found_resp(modelID):
     msg = "No model with id '%s' exists" % modelID
     return dict(message=msg), 404
 
+
+def _parse_model_bson(bson_bytes):
+    try:
+        mod_dict = bson.loads(bson_bytes)
+    except Exception:
+        raise ParseException("malformed bson data")
+
+    return mod_dict
+
+
 def new(model):
-    mod = Model.from_dict(_parse_model_bson(model))
+    try:
+        mod = Model.from_dict(_parse_model_bson(model))
+    except ParseException as e:
+        return _invalid_model_resp(e)
+
     database.db_session.add(mod)
     database.db_session.commit()
 

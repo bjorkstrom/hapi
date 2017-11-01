e82bdd4d8c43435f08f69e4a7289b2b425bb7a8a
elmir <elmir@brab>
2017-11-01 07:37:01 +0100

    asdf

--- host/models.py
+++ host/models.py
@@ -8,6 +8,10 @@ def _parse_model_bson(bson_bytes):
     return bson.loads(bson_bytes)
 
 
+def _model_not_found_resp(modelID):
+    msg = "No model with id '%s' exists" % modelID
+    return dict(message=msg), 404
+
 def new(model):
     mod = Model.from_dict(_parse_model_bson(model))
     database.db_session.add(mod)
@@ -18,6 +22,9 @@ def new(model):
 
 def update(modelID, body):
     mod = Model.get(modelID)
+    if mod is None:
+        return _model_not_found_resp(modelID)
+
     mod.update(body)
     database.db_session.commit()
 
@@ -25,11 +32,18 @@ def update(modelID, body):
 
 
 def get(modelID):
+    mod = Model.get(modelID)
+    if mod is None:
+        return _model_not_found_resp(modelID)
+
     return Model.get(modelID).as_dict()
 
 
 def delete(modelID):
     mod = Model.get(modelID)
+    if mod is None:
+        return _model_not_found_resp(modelID)
+
     database.db_session.delete(mod)
     database.db_session.commit()
 

cf068713ba43dec70a1af9c4a0a272c1ac1c7945
Noor <noorharbaji@outlook.sa>
2017-12-12 15:14:28 +0100

    Resend event if error hapen

--- server/hapi/eventd.py
+++ server/hapi/eventd.py
@@ -4,9 +4,11 @@ import json
 import time
 from hapi.dbmodels import Device
 import requests
+import threading
+from queue import Queue
 
 EVENTS_QUEUE = "events"
-
+queue = Queue(maxsize=0)
 
 def do_maintenance():
     # maintenance hook, e.g. remove expired subscriptions, TBD
@@ -33,9 +35,27 @@ def send_event(device, topic, restEndpoint, timestamp):
     headers = {}
     for header in restEndpoint.httpheaders:
         headers[header.name] = header.value
+    for _ in range(3):
+        if reSend_event(restEndpoint.url, payload, headers):
+            break
 
-    r = requests.post(restEndpoint.url, json=payload, headers=headers)
-    print(r)
+
+#Resend if there is any error
+def reSend_event(url, payload, headers):
+    try:
+        r = requests.post(url, json=payload, headers=headers)
+        r.raise_for_status()
+        print(r)
+        return True
+    except requests.exceptions.RequestException as err:
+        print ("OOps: Something Else",err)
+    except requests.exceptions.HTTPError as errh:
+        print ("Http Error:",errh)
+    except requests.exceptions.ConnectionError as errc:
+        print ("Error Connecting:",errc)
+    except requests.exceptions.Timeout:
+        print ("Timeout Error:",errt)
+    return False
 
 
 def dispatch_event(device, topic, payload, timestamp):
@@ -62,16 +82,30 @@ def parse_body(body):
     return ed["device"], ed["topic"], None, ed["timestamp"]
 
 
+def sender_thread(events_queue):
+    while True:
+        data = events_queue.get()
+        print("sender thread %s", data)
+        dispatch_event(*data)
+        
+
 def main():
     channel = init_channel()
+    data = []
 
+    t = threading.Thread(target=sender_thread, args = (queue, ))
+    t.start()
+    
     for msg in channel.consume(EVENTS_QUEUE, inactivity_timeout=400):
         if msg is None:
             do_maintenance()
             continue
-
         method, properties, body = msg
-        dispatch_event(*parse_body(body))
+        data = parse_body(body)
+        print("main thread %s", data)
+        queue.put(data)
+        print(".........................................................")
+        print("End")
         channel.basic_ack(method.delivery_tag)
 
 

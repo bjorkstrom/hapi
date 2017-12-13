01cf651288339d01867758a40f5f0b4a58048249
Noor <noorharbaji@outlook.sa>
2017-12-13 10:40:15 +0100

    Support payload

--- server/hapi/eventd.py
+++ server/hapi/eventd.py
@@ -4,8 +4,11 @@ import json
 import time
 from hapi.dbmodels import Device
 import requests
+import threading
+from queue import Queue
 
 EVENTS_QUEUE = "events"
+queue = Queue(maxsize=0)
 
 
 def do_maintenance():
@@ -20,22 +23,45 @@ def epoch2iso(epoch):
     return time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime(epoch))
 
 
-def send_event(device, topic, restEndpoint, timestamp):
+def send_event(device, topic, restEndpoint, timestamp, payload):
     """
     send the event to subscription's destination
     """
-    payload = {
+    request_body = {
         "topic": topic,
         "device": device,
-        "timestamp": epoch2iso(timestamp)
+        "timestamp": epoch2iso(timestamp),
     }
+
+    if payload is not None:
+        request_body["description"] = payload
 
     headers = {}
     for header in restEndpoint.httpheaders:
         headers[header.name] = header.value
+    for _ in range(3):
+        if reSend_event(restEndpoint.url, request_body, headers):
+            break
 
-    r = requests.post(restEndpoint.url, json=payload, headers=headers)
-    print(r)
+
+def reSend_event(url, request_body, headers):
+    """
+    Send event or resend it if there is an error
+    """
+    try:
+        r = requests.post(url, json=request_body, headers=headers)
+        r.raise_for_status()
+        print(r)
+        return True
+    except requests.exceptions.RequestException as err:
+        print("OOps: Something Else", err)
+    except requests.exceptions.HTTPError as errh:
+        print("Http Error:", errh)
+    except requests.exceptions.ConnectionError as errc:
+        print("Error Connecting:", errc)
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error:", errt)
+    return False
 
 
 def dispatch_event(device, topic, payload, timestamp):
@@ -45,7 +71,7 @@ def dispatch_event(device, topic, payload, timestamp):
         for subTopic in sub.topics:
             if subTopic.topic == topic:
                 print("send event: device %s, topic %s" % (device, topic))
-                send_event(device, topic, sub.restEndpoint, timestamp)
+                send_event(device, topic, sub.restEndpoint, timestamp, payload)
 
 
 def init_channel():
@@ -59,19 +85,31 @@ def init_channel():
 
 def parse_body(body):
     ed = json.loads(body.decode("utf-8"))
-    return ed["device"], ed["topic"], None, ed["timestamp"]
+    return ed["device"], ed["topic"], ed.get("payload"), ed["timestamp"]
+
+
+def sender_thread(events_queue):
+    while True:
+        data = events_queue.get()
+        print("sender thread %s", data)
+        dispatch_event(*data)
 
 
 def main():
     channel = init_channel()
+    data = []
+
+    t = threading.Thread(target=sender_thread, args=(queue, ))
+    t.start()
 
     for msg in channel.consume(EVENTS_QUEUE, inactivity_timeout=400):
         if msg is None:
             do_maintenance()
             continue
-
         method, properties, body = msg
-        dispatch_event(*parse_body(body))
+        data = parse_body(body)
+        print("main thread %s", data)
+        queue.put(data)
         channel.basic_ack(method.delivery_tag)
 
 
